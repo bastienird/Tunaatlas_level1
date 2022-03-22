ff53cf4aebfa79e2c6a43a8884e1679cab3e5680
BastienIRD <bastien.grasset@ird.fr>
2022-03-22 19:43:02 +0100

    fixing mistake for directory writing csv

--- fonction_dossier.R
+++ fonction_dossier.R
@@ -2,7 +2,7 @@ fonction_dossier=function(nom_dossier, nomrds, explication="",fonctions="", opti
   dir.create(nom_dossier)
   somme_t <- sum((nomrds %>% filter(unit %in% c("t", "MTNO","MT")))$value, na.rm = TRUE)
   somme_no <- sum((nomrds %>% filter(unit %in% c("no", "NOMT","NO")))$value, na.rm = TRUE)
-  write_csv(data.frame(somme_t, somme_no), paste0(nom_dossier,"sums.csv"))
+  write_csv(data.frame(somme_t, somme_no), paste0(nom_dossier,"/sums.csv"))
   saveRDS(nomrds,paste0(nom_dossier,"/rds.rds"))
   write(explication, paste0(nom_dossier,"/explication.txt")) 
   write(fonctions, paste0(nom_dossier,"/fonctions.txt"))
