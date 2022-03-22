d048cf59e30819f42d91b29b980aab443ad955af
BastienIRD <bastien.grasset@ird.fr>
2022-03-22 18:36:22 +0100

    ajout fonction creation dossier

---
+++ fonction_dossier.R
@@ -0,0 +1,9 @@
+fonction_dossier=function(nom_dossier, nomrds, explication,fonctions) {
+  dir.create(nom_dossier)
+  saveRDS(nomrds,paste0(nom_dossier,"/rds.rds"))
+  write(explication, paste0(nom_dossier,"/explication.txt")) 
+  write(fonctions, paste0(nom_dossier,"/fonctions.txt"))
+
+}
+
+test <- as.data.frame(iris)
