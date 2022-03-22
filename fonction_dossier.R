348d3d5452e07af776c7e4e69eea41c063e1a3e5
BastienIRD <bastien.grasset@ird.fr>
2022-03-22 18:49:32 +0100

    adding options txt

--- fonction_dossier.R
+++ fonction_dossier.R
@@ -1,8 +1,9 @@
-fonction_dossier=function(nom_dossier, nomrds, explication,fonctions) {
+fonction_dossier=function(nom_dossier, nomrds, explication,fonctions, options) {
   dir.create(nom_dossier)
   saveRDS(nomrds,paste0(nom_dossier,"/rds.rds"))
   write(explication, paste0(nom_dossier,"/explication.txt")) 
   write(fonctions, paste0(nom_dossier,"/fonctions.txt"))
-
+  write(options, paste0(nom_dossier,"/options.txt"))
+  
 }
 
