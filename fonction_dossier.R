b44311aa341eae337f10ea0acca9cdfab7cc2a95
BastienIRD <bastien.grasset@ird.fr>
2022-03-24 17:12:54 +0100

    Adding function to make it easier to paste options

--- fonction_dossier.R
+++ fonction_dossier.R
@@ -1,10 +1,20 @@
-fonction_dossier=function(nom_dossier, nomrds, explication="",fonctions="", options="") {
+fonction_option = function(x){
+  value <- eval(as.symbol(x))
+  paste0(x, "= ", value)
+}
+
+function_option_assign = function(x){
+  t <- lapply(x, fonction_option)
+}
+
+fonction_dossier=function(nom_dossier, nomrds, explication="",fonctions=c(""), options="") {
   dir.create("Markdown")
   dir.create(paste0("Markdown/",nom_dossier))
   nom_dossier <- paste0("Markdown/",nom_dossier)
   somme_t <- sum((nomrds %>% filter(unit %in% c("t", "MTNO","MT")))$value, na.rm = TRUE)
   somme_no <- sum((nomrds %>% filter(unit %in% c("no", "NOMT","NO")))$value, na.rm = TRUE)
   write_csv(data.frame(somme_t, somme_no), paste0(nom_dossier,"/sums.csv"))
+  fonctions <- function_option_assign(fonctions)
   saveRDS(nomrds,paste0(nom_dossier,"/rds.rds"))
   write(explication, paste0(nom_dossier,"/explication.txt")) 
   write(fonctions, paste0(nom_dossier,"/fonctions.txt"))
