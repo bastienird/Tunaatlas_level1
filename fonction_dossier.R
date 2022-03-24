992b6c37fd1eb1166984b1daf80ff84569a8a40a
BastienIRD <bastien.grasset@ird.fr>
2022-03-24 17:52:21 +0100

    upgrading the printing of options + fixing the confusion between options and function

--- fonction_dossier.R
+++ fonction_dossier.R
@@ -1,21 +1,20 @@
 fonction_option = function(x){
   value <- eval(as.symbol(x))
-  paste0(x, "= ", value)
+  paste0(x, " = ", value)
 }
 
 function_option_assign = function(x){
-  t <- lapply(x, fonction_option)
-  print(t)
+  paste(paste0(lapply(x, fonction_option)), collapse = " ; ")
 }
 
-fonction_dossier=function(nom_dossier, nomrds, explication="",fonctions=c(""), options="") {
+fonction_dossier=function(nom_dossier, nomrds, explication="",fonctions="", options=c("")) {
   dir.create("Markdown")
   dir.create(paste0("Markdown/",nom_dossier))
   nom_dossier <- paste0("Markdown/",nom_dossier)
   somme_t <- sum((nomrds %>% filter(unit %in% c("t", "MTNO","MT")))$value, na.rm = TRUE)
   somme_no <- sum((nomrds %>% filter(unit %in% c("no", "NOMT","NO")))$value, na.rm = TRUE)
   write_csv(data.frame(somme_t, somme_no), paste0(nom_dossier,"/sums.csv"))
-  fonctions <- function_option_assign(fonctions)
+  options <- function_option_assign(options)
   saveRDS(nomrds,paste0(nom_dossier,"/rds.rds"))
   write(explication, paste0(nom_dossier,"/explication.txt")) 
   write(fonctions, paste0(nom_dossier,"/fonctions.txt"))
