6131dbc3a75592bdc2a4d1878ea08f7263b536f8
BastienIRD <bastien.grasset@ird.fr>
2022-03-29 00:25:10 +0200

    seems not to be pushed on github, I try again

--- fonction_dossier.R
+++ fonction_dossier.R
@@ -6,13 +6,19 @@ fonction_dossier=function(nom_dossier, nomrds, explication="",fonctions="", opti
   somme_no <- sum((nomrds %>% filter(unit %in% c("no", "NOMT","NO")))$value, na.rm = TRUE)
   write_csv(data.frame(somme_t, somme_no), paste0(nom_dossier,"/sums.csv"))
   if (!is.null(options)){
-  options <-   paste(paste0(substitute(options), " = ",options), collapse = " ; ")
-  } else {options = "NONE"}
-
+    options_substi <- as.list(substitute(options))[-1]
+    options_written <- ""
+    for (i in 1:length(options_substi)){
+      options_written <- paste(options_written, (paste0(options_substi[i], " = ", options[i])), 
+                               collapse = " ; ")
+      
+    }
+    } else {options = "NONE"}
+    
   saveRDS(nomrds,paste0(nom_dossier,"/rds.rds"))
   write(explication, paste0(nom_dossier,"/explication.txt")) 
   write(fonctions, paste0(nom_dossier,"/fonctions.txt"))
-  write(options, paste0(nom_dossier,"/options.txt"))
+  write(options_written, paste0(nom_dossier,"/options.txt"))
   
 }
 
