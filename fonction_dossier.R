bd1bc648bd0a72b667dc5c5c55ffb2cdcdc45b1c
BastienIRD <bastien.grasset@ird.fr>
2022-03-30 13:46:21 +0200

    adding lines numbers for the final table

--- fonction_dossier.R
+++ fonction_dossier.R
@@ -2,9 +2,12 @@ fonction_dossier=function(nom_dossier, nomrds, explication="",fonctions="", opti
   dir.create("Markdown")
   dir.create(paste0("Markdown/",nom_dossier))
   nom_dossier <- paste0("Markdown/",nom_dossier)
-  somme_t <- sum((nomrds %>% filter(unit %in% c("t", "MTNO","MT")))$value, na.rm = TRUE)
-  somme_no <- sum((nomrds %>% filter(unit %in% c("no", "NOMT","NO")))$value, na.rm = TRUE)
-  write_csv(data.frame(somme_t, somme_no), paste0(nom_dossier,"/sums.csv"))
+  rds_t <- (nomrds %>% filter(unit %in% c("t", "MTNO","MT"))) 
+  rds_t <- (nomrds %>% filter(unit %in% c("no", "NOMT","NO"))) 
+  somme_t <- sum(rds_t$value, na.rm = TRUE)
+  somme_no <- sum(rds_no$value, na.rm = TRUE)
+  lines <- nrow(nomrds)
+  write_csv(data.frame(somme_t, somme_no, lines), paste0(nom_dossier,"/sums.csv"))
   if (!is.null(options)){
     options_substi <- as.list(substitute(options))[-1]
     options_written <- ""
