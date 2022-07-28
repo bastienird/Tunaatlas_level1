e969d0408117c64a8570d74bba91b1fd439cfdd9
BastienGrassetIRD <bastien.grasset@ird.fr>
2022-07-28 14:38:02 +0200

    chanign name of default argument

--- level1_changing_order.R
+++ level1_changing_order.R
@@ -94,9 +94,9 @@ last_path = function(x){tail(str_split(x,"/")[[1]],n=1)}
 
 # source("https://raw.githubusercontent.com/BastienIRD/Tunaatlas_level1/main/comp_sans_shiny.Rmd")
 # step_for_rmd <- 1
-create_latex = function(x,last = FALSE,unique = FALSE, rawdataneeded = FALSE, config= config){
+create_latex = function(x,last = FALSE,unique = FALSE, rawdataneeded = FALSE, config2 = config){
   last_path = function(x){tail(str_split(x,"/")[[1]],n=1)}
-  
+  config <- config2
   wd <- getwd()
   # config$logger.info(paste0("working direcotyr begining create_latex : ",wd))
   # y <- last_path(x)
@@ -131,12 +131,13 @@ create_latex = function(x,last = FALSE,unique = FALSE, rawdataneeded = FALSE, co
                    x), paste0(wd,"/",name_output,x), overwrite = TRUE)
   # setwd(paste0(wd,"/",output_file))
   # conection_db <- postgresqlConnectionInfo(con)
+  print(config$software$output$dbi)
   if(unique == TRUE){rmarkdown::render(paste0(name_output,x),
                                        params = list(final = last, host = config$software$input$dbi_config$parameters$host, 
                                                                            port = config$software$input$dbi_config$parameters$port, 
                                                                            user = config$software$input$dbi_config$parameters$user,
                                                                            dbname=config$software$input$dbi_config$parameters$dbname,
-                                                     password = config$software$input$dbi_config$parameters$password, con =   config$software$output$dbi
+                                                     password = config$software$input$dbi_config$parameters$password, con = config$software$output$dbi
 
                                                                            ))}
   if(unique==FALSE){rmarkdown::render(paste0(name_output,x),params = list(init = avant_last, final = last, host = config$software$input$dbi_config$parameters$host, 
