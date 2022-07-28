a487cbde015ef18f56fcc972d5b9c9407c3a2aa4
BastienGrassetIRD <bastien.grasset@ird.fr>
2022-07-28 14:14:02 +0200

    config in function

--- level1_changing_order.R
+++ level1_changing_order.R
@@ -94,7 +94,7 @@ last_path = function(x){tail(str_split(x,"/")[[1]],n=1)}
 
 # source("https://raw.githubusercontent.com/BastienIRD/Tunaatlas_level1/main/comp_sans_shiny.Rmd")
 # step_for_rmd <- 1
-create_latex = function(x,last = FALSE,unique = FALSE, rawdataneeded = FALSE, con= con){
+create_latex = function(x,last = FALSE,unique = FALSE, rawdataneeded = FALSE, config= config){
   last_path = function(x){tail(str_split(x,"/")[[1]],n=1)}
   
   wd <- getwd()
@@ -110,7 +110,7 @@ create_latex = function(x,last = FALSE,unique = FALSE, rawdataneeded = FALSE, co
     # file.copy(paste0(wd2,"/",
     # x), paste0(wd,"/",x), overwrite = TRUE)
   # config$logger.info(paste0("jsute after copying rmd"))
-  
+
   file.copy(paste0(wd2,"/",'data2/SPECIES_LIST_RFMO_WITH_ERRORS.xlsx'),paste0(wd,"/",'data/SPECIES_LIST_RFMO_WITH_ERRORS.xlsx') , overwrite = TRUE)
   file.copy(paste0(wd2,"/",'data2/cl_cwp_gear_level2.csv'),paste0(wd,"/",'data/cl_cwp_gear_level2.csv') , overwrite = TRUE)
   if(!rawdataneeded == FALSE){
@@ -136,13 +136,14 @@ create_latex = function(x,last = FALSE,unique = FALSE, rawdataneeded = FALSE, co
                                                                            port = config$software$input$dbi_config$parameters$port, 
                                                                            user = config$software$input$dbi_config$parameters$user,
                                                                            dbname=config$software$input$dbi_config$parameters$dbname,
-                                                     password = config$software$input$dbi_config$parameters$password, con = con
+                                                     password = config$software$input$dbi_config$parameters$password, con =   config$software$output$dbi
+
                                                                            ))}
   if(unique==FALSE){rmarkdown::render(paste0(name_output,x),params = list(init = avant_last, final = last, host = config$software$input$dbi_config$parameters$host, 
                                                                                         port = config$software$input$dbi_config$parameters$port, 
                                                                                         user = config$software$input$dbi_config$parameters$user,
                                                                                         dbname=config$software$input$dbi_config$parameters$dbname,
-                                                                                        password = config$software$input$dbi_config$parameters$password, con = con
+                                                                                        password = config$software$input$dbi_config$parameters$password, con = config$software$output$dbi
                                                                           ))}#,
   #output_file = paste0(gsub(".Rmd", "",x), "step",step_for_rmd,".Rmd")
   
