a3d267045988a1aabf407773253e42a5511db73d
BastienGrassetIRD <bastien.grasset@ird.fr>
2022-07-28 12:08:59 +0200

    adding writing of option for each markdown

--- fonction_dossier.R
+++ fonction_dossier.R
@@ -1,32 +1,32 @@
-function_creation_options = function(){
+# function_creation_options = function(){
+# 
+# j <- 1
+# 
+# list_options <-assign("list_options", data.frame(matrix(ncol =2 , nrow = 1)), envir= .GlobalEnv)
+# colnames(list_options) <- c("Options", "Position")
+# 
+# 
+# for (i in names(options)){
+#   if (i != ""){
+#     
+#     assign(paste0("options_",i), paste(options[[j]], collapse = ' ; '), envir= .GlobalEnv)
+#     assign(i, paste0(options[[j]]), envir= .GlobalEnv)}
+#   if (options[[j]][1] == TRUE){
+#     assign(i, options[[j]], envir= .GlobalEnv)
+#   } else if (options[[j]][1] == FALSE){
+#     assign(i, options[[j]], envir= .GlobalEnv)
+#   } 
+#   assign("data_i",  data.frame(i, paste(options[[j]], collapse = ' ; ')))
+#   names(data_i) <- colnames(list_options)
+#   assign("data_i",data_i, envir= .GlobalEnv)
+#   list_options <- rbind(list_options, data_i)
+#   
+#   j <-  j+1 
+# }
+# list_options = list_options[-1,]
+# }
 
-j <- 1
-
-list_options <-assign("list_options", data.frame(matrix(ncol =2 , nrow = 1)), envir= .GlobalEnv)
-colnames(list_options) <- c("Options", "Position")
-
-
-for (i in names(options)){
-  if (i != ""){
-    
-    assign(paste0("options_",i), paste(options[[j]], collapse = ' ; '), envir= .GlobalEnv)
-    assign(i, paste0(options[[j]]), envir= .GlobalEnv)}
-  if (options[[j]][1] == TRUE){
-    assign(i, options[[j]], envir= .GlobalEnv)
-  } else if (options[[j]][1] == FALSE){
-    assign(i, options[[j]], envir= .GlobalEnv)
-  } 
-  assign("data_i",  data.frame(i, paste(options[[j]], collapse = ' ; ')))
-  names(data_i) <- colnames(list_options)
-  assign("data_i",data_i, envir= .GlobalEnv)
-  list_options <- rbind(list_options, data_i)
-  
-  j <-  j+1 
-}
-list_options = list_options[-1,]
-}
-
-fonction_dossier=function(nom_dossier, nomrds, explication="",fonctions="", options=NULL) {
+fonction_dossier=function(nom_dossier, nomrds, explication="",fonctions="", option_list=NULL) {
   if(!exists("options_written_total")){assign("options_written_total", "", envir = .GlobalEnv)}
   dir.create("Markdown")
   dir.create(paste0("Markdown/",nom_dossier))
@@ -37,12 +37,12 @@ fonction_dossier=function(nom_dossier, nomrds, explication="",fonctions="", opti
   somme_no <- sum(rds_no$value, na.rm = TRUE)
   lines <- nrow(nomrds)
   write_csv(data.frame(somme_t, somme_no, lines), paste0(nom_dossier,"/sums.csv"))
-  if (!is.null(options)){
-    options_substi <- as.list(substitute(options))[-1]
+  if (!is.null(option_list)){
+    options_substi <- as.list(substitute(option_list))[-1]
     print(options_substi)
     options_written <- ""
     for (i in 1:length(options_substi)){
-      options_written <- paste0(options_written, (paste0(options_substi[i], " = ", options[i])), 
+      options_written <- paste0(options_written, (paste0(options_substi[i], " = ", option_list[i])), 
                                 sep = " , \n ")
       
       
@@ -52,12 +52,13 @@ fonction_dossier=function(nom_dossier, nomrds, explication="",fonctions="", opti
     }
     print(options_written)
   } else {options_written = "NONE"}
-  
+  options_written_total <- assign("options_written_total", paste0(options_written_total, options_written), envir = .GlobalEnv)
   saveRDS(nomrds,paste0(nom_dossier,"/rds.rds"))
   write(explication, paste0(nom_dossier,"/explication.txt")) 
   write(fonctions, paste0(nom_dossier,"/fonctions.txt"))
-  write(options_written_total, paste0("options_total.txt"))
+  write(options_written_total, "options_total.txt")
   write(options_written_total, paste0(nom_dossier,"/options_total.txt"))
+  write(options_written, paste0(nom_dossier,"/options_written.txt"))
   
 }
 
