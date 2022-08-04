669cbf4c50eccea86c2f7be194a95ad12d7b6a68
BastienGrassetIRD <bastien.grasset@ird.fr>
2022-08-04 15:55:18 +0200

    error

--- level1_changing_order.R
+++ level1_changing_order.R
@@ -275,8 +275,9 @@ fonction_dossier("rawdata",
                                                   options_iattc_ps_raise_flags_to_schooltype,
                                                   options_iattc_ps_dimension_to_use_if_no_raising_flags_to_schooltype,
                                                   options_iccat_ps_include_type_of_school))
-create_latex("absurd_data.Rmd", unique = TRUE)
 saveRDS(georef_dataset, "data/rawdata.rds")
+create_latex("absurd_data.Rmd", unique = TRUE)
+
 unlink("Markdown")
 
 query <- "SELECT  code,code_cwp from area.irregular_areas_task2_iotc"
