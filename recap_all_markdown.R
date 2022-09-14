cab5dc1d2499f80c5156a58aed9ad1bbaf09dc30
BastienGrassetIRD <bastien.grasset@ird.fr>
2022-09-14 16:37:55 +0200

    launching markdown global

---
+++ recap_all_markdown.R
@@ -0,0 +1,13 @@
+comparison_each_step <- function(action, entity, config){
+  rmarkdown::render(use_github_file(
+    "https://raw.githubusercontent.com/BastienIRD/Tunaatlas_level1/main/tableau_recap_global_action.Rmd",
+    path = NULL,
+    save_as = "recap_all.Rmd",
+    ref = NULL,
+    ignore = FALSE,
+    open = TRUE,
+    host = NULL
+  ), 
+  params = list(action = action,
+                entity = entity, config = config))
+}
