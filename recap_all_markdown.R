b0938b6fed2fa843bf1cbb63a4ac7dcbdee74fbb
BastienGrassetIRD <bastien.grasset@ird.fr>
2022-09-14 16:41:52 +0200

    adding options parameters

--- recap_all_markdown.R
+++ recap_all_markdown.R
@@ -1,4 +1,4 @@
-comparison_each_step <- function(action, entity, config){
+comparison_each_step <- function(action,entity, config, options){
   rmarkdown::render(use_github_file(
     "https://raw.githubusercontent.com/BastienIRD/Tunaatlas_level1/main/tableau_recap_global_action.Rmd",
     path = NULL,
