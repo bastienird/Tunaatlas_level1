9c107184989f14dbc84ffb3a006e81dbd475f23a
BastienGrassetIRD <bastien.grasset@ird.fr>
2022-09-14 20:00:13 +0200

    adding downloading of all child.Rmd

--- recap_all_markdown.R
+++ recap_all_markdown.R
@@ -1,13 +1,23 @@
-comparison_each_step <- function(action,entity, config, options){
-  rmarkdown::render(use_github_file(
-    "https://raw.githubusercontent.com/BastienIRD/Tunaatlas_level1/main/tableau_recap_global_action.Rmd",
-    path = NULL,
-    save_as = "recap_all.Rmd",
-    ref = NULL,
-    ignore = FALSE,
-    open = TRUE,
-    host = NULL
-  ), 
+comparison_each_step <- function(action, entity, config, options){
+  if(!(require(here))){ 
+    install.packages("here") 
+    (require(here))} 
+  copyrmd <- function(x){
+    last_path = function(y){tail(str_split(y,"/")[[1]],n=1)}
+    use_github_file(repo_spec =x,
+                    save_as = paste0(gsub(as.character(here::here()),"",as.character(getwd())), paste0("/", last_path(x))),
+                    ref = NULL,
+                    ignore = FALSE,
+                    open = FALSE,
+                    host = NULL
+    ) }
+  
+  c <- c("https://raw.githubusercontent.com/BastienIRD/Tunaatlas_level1/main/tableau_recap_global_action.Rmd", 
+         "https://raw.githubusercontent.com/BastienIRD/Tunaatlas_level1/main/Analyse_georeferenced_child.Rmd",
+         "https://raw.githubusercontent.com/BastienIRD/Tunaatlas_level1/main/comp_sans_shiny_child.Rmd")
+  lapply(c,copyrmd)
+  rmarkdown::render("tableau_recap_global_action.Rmd"  , 
   params = list(action = action,
                 entity = entity, config = config))
 }
+
