665e6fad09e2da682089c88fc29029222ef4404b
BastienGrassetIRD <bastien.grasset@ird.fr>
2022-11-30 17:06:28 +0100

    path error

--- map_codelists_Bastien.R
+++ map_codelists_Bastien.R
@@ -1,7 +1,7 @@
 map_codelistsB = function(con, fact, mapping_dataset,dataset_to_map, mapping_keep_src_code = FALSE, summary_mapping = FALSE){
   # Get the dimensions to map from the mapping_dataset
   source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/map_codelist.R")
-  source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/extract_dataset.R")
+  source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/sardara_functions/extract_dataset.R")
   source("https://raw.githubusercontent.com/firms-gta/geoflow-tunaatlas/master/sardara_functions/list_metadata_datasets.R")
   
   if (fact=="catch"){
